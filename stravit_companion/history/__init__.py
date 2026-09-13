33066d4eaa95c4c753786b5591dee091786c9ed8
Mario Wu <mariusz.wincior@gmail.com>
2026-09-14 00:31:03 +0200

    feat: add leaderboard history queries (#22)

---
+++ stravit_companion/history/__init__.py
@@ -0,0 +1,13 @@
+from stravit_companion.history.service import (
+    LeaderboardHistory,
+    ParticipantHistory,
+    ParticipantHistoryPoint,
+    get_leaderboard_history,
+)
+
+__all__ = [
+    "LeaderboardHistory",
+    "ParticipantHistory",
+    "ParticipantHistoryPoint",
+    "get_leaderboard_history",
+]
